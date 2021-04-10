5ce7e494181f4c509c7998fada54b0b71bf0d1c9
lahiru prabodha <67253461+Lprabodha@users.noreply.github.com>
2021-04-10 19:54:02 +0530

    react
    react_fundaments

--- src/components/NameList/NameListItem.jsx
+++ src/components/NameList/NameListItem.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import moment from 'moment';
+import './NameListItem.css';
 
 
 function NameListItem(props){
     return(
         <li>
-            <p><img src={props.avatar}/> alt={props.name} {props.name}</p>
+            <p style={{color:'green'}}>
+                <img src={props.avatar} alt={props.name} />{props.name}</p>
             <p>City:{props.city}</p>
             <p>Email:{props.email}</p>
             <p>Bithday:{moment(props.birthday).format('DD-MM-YYYY')}</p>
